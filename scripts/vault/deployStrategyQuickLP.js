644138e26f446749707cafb58961c5ba55ffc6ae
ABeely <ashleybeel@icloud.com>
2022-01-20 17:19:42 +0000

    Update deployStrategyQuickLP.js

--- scripts/vault/deployStrategyQuickLP.js
+++ scripts/vault/deployStrategyQuickLP.js
@@ -7,50 +7,94 @@ import { BeefyChain } from "../../utils/beefyChain";
 
 const registerSubsidy = require("../../utils/registerSubsidy");
 
-// Below outputs the token and contract addresses.
-// Saves them to the variables highlighted in red.
 const {
-  platforms: { quickswap, beefyfinance },
+  platforms: { beefyfinance, quickswap, sushi },
   tokens: {
-    DAI: { address: DAI },
-    QUICK: { address: QUICK },
     MATIC: { address: MATIC },
     ETH: { address: ETH },
   },
 } = addressBook.polygon;
 
-// As GNS is not currently within the address book I will deploy it using the address here
-const GNS = web3.utils.toChecksumAddress("0xE5417Af564e4bFDA1c483642db72007871397896");
+// const GENESIS = web3.utils.toChecksumAddress("0x51869836681bce74a514625c856afb697a013797");
+// const FODL = web3.utils.toChecksumAddress("0x5314bA045a459f63906Aa7C76d9F337DcB7d6995");
+const ICE = web3.utils.toChecksumAddress("0x4e1581f01046eFDd7a1a2CDB0F82cdd7F71F2E59");
 
-const shouldVerifyOnEtherscan = false;
+const changeableParams = {
+  mooName: "Moo Popsicle ICE-WETH",
+  mooSymbol: "mooPopsicleICE-WETH",
+  want: web3.utils.toChecksumAddress("0x941eb28e750C441AEF465a89E43DDfec2561830b"),
+  poolId: 0,
+  chef: web3.utils.toChecksumAddress("0xbf513aCe2AbDc69D38eE847EFFDaa1901808c31c"),
+  // rewardPool: web3.utils.toChecksumAddress("0x3620418dD43853c35fF8Df90cAb5508FB5df46Bf"),
+  outputToNativeRoute: [ICE, ETH, MATIC],
+  // rewardToNativeRoute: [],
+  outputToLp0Route: [ICE],
+  outputToLp1Route: [ICE, ETH],
+  // nativeToLp0Route: [],
+  // nativeToLp1Route: [],
+  strategyName: "StrategyCommonChefLP",
+  router: sushi.router,
+  deployToProd: true,
+  shouldHarvestOnDeposit: true,
+};
 
-// Quickswap reward pool
-const rewardPool = web3.utils.toChecksumAddress("0x33025b177A35F6275b78f9c25684273fc24B4e43");
+// Currently Broken
+// const changeableParams = {
+//   mooName: "Moo Quick MATIC-FODL",
+//   mooSymbol: "mooQuickMATIC-FODL",
+//   want: web3.utils.toChecksumAddress("0x2Fc4DFCEe8C331D54341f5668a6d9BCdd86F8e2f"),
+//   rewardPool: web3.utils.toChecksumAddress("0x3620418dD43853c35fF8Df90cAb5508FB5df46Bf"),
+//   outputToNativeRoute: [QUICK, MATIC],
+//   rewardToNativeRoute: [FODL, USDC, MATIC],
+//   nativeToLp0Route: [MATIC],
+//   nativeToLp1Route: [MATIC, FODL],
+//   strategyName: "StrategyQuickswapDualRewardLP",
+//   router: quickswap.router,
+//   deployToProd: false,
+//   shouldHarvestOnDeposit: true,
+// };
 
-// Used to create the Beefy Vault Contract
+// const changeableParams = {
+//   mooName: "Moo Quick QUICK-GENESIS",
+//   mooSymbol: "mooQuickQUICK-GENESIS",
+//   want: web3.utils.toChecksumAddress("0xF0696be85fa54F7a8C9F20AA98aA4409CD5C9D1B"),
+//   rewardPool: web3.utils.toChecksumAddress("0x3620418dD43853c35fF8Df90cAb5508FB5df46Bf"),
+//   outputToNativeRoute: [QUICK, MATIC],
+//   rewardToNativeRoute: [GENESIS, QUICK, MATIC],
+//   nativeToLp0Route: [MATIC, QUICK, GENESIS],
+//   nativeToLp1Route: [MATIC, QUICK],
+//   strategyName: "StrategyQuickswapDualRewardLP",
+//   router: quickswap.router,
+//   deployToProd: false,
+// };
+
+const shouldVerifyOnEtherscan = changeableParams.deployToProd;
+
 const vaultParams = {
-  mooName: "Moo Quick-GNS-DAI",
-  mooSymbol: "mooQuickGnsDai",
+  mooName: changeableParams.mooName,
+  mooSymbol: changeableParams.mooSymbol,
   delay: 21600,
 };
 
 // Used to create the Vault Strategy
 const strategyParams = {
-  want: "0x6e53cb6942e518376e9e763554db1a45ddcd25c4", // LP token pool
-  rewardPool: rewardPool, // LP Farm / Reward pool
-  unirouter: quickswap.router, // Router
+  want: changeableParams.want,
+  // rewardPool: changeableParams.rewardPool,
+  unirouter: changeableParams.router,
   strategist: "0x5EAeA735914bf0766428C85a20429020ba688130", // my address
-  // keeper: beefyfinance.keeper, // Address of keeper (Able to panic / pause / unpause vaults)
-  keeper: "0xD3425091b74bd097f6d8f194D30229140F814F14",
+  keeper: changeableParams.deployToProd ? beefyfinance.keeper : "0xD3425091b74bd097f6d8f194D30229140F814F14", // Address of keeper (Able to panic / pause / unpause vaults)
   beefyFeeRecipient: beefyfinance.beefyFeeRecipient,
-  outputToNativeRoute: [QUICK, MATIC],
-  outputToLp0Route: [QUICK, ETH, DAI],
-  outputToLp1Route: [QUICK, ETH, DAI, GNS],
+  outputToNativeRoute: changeableParams.outputToNativeRoute,
+  outputToLp0Route: changeableParams.outputToLp0Route,
+  outputToLp1Route: changeableParams.outputToLp1Route,
+  // rewardToNativeRoute: changeableParams.rewardToNativeRoute,
+  // nativeToLp0Route: changeableParams.nativeToLp0Route,
+  // nativeToLp1Route: changeableParams.nativeToLp1Route,
 };
 
 const contractNames = {
   vault: "BeefyVaultV6",
-  strategy: "StrategyPolygonQuickLP",
+  strategy: changeableParams.strategyName,
 };
 
 async function main() {
@@ -74,7 +118,6 @@ async function main() {
   console.log("Deploying:", vaultParams.mooName);
 
   const predictedAddresses = await predictAddresses({ creator: deployer.address });
-  console.log(predictAddresses);
 
   const vaultConstructorArguments = [
     predictedAddresses.strategy,
@@ -89,7 +132,8 @@ async function main() {
 
   const strategyConstructorArguments = [
     strategyParams.want,
-    strategyParams.rewardPool,
+    changeableParams.poolId,
+    changeableParams.chef,
     vault.address,
     strategyParams.unirouter,
     strategyParams.keeper,
@@ -106,10 +150,7 @@ async function main() {
   console.log();
   console.log("Vault:", vault.address);
   console.log("Strategy:", strategy.address);
-  console.log("Keeper:", strategy.keeper);
   console.log("Want:", strategyParams.want);
-  console.log("RewardPool:", strategyParams.rewardPool);
-
   console.log();
   console.log("Running post deployment");
 
@@ -123,6 +164,10 @@ async function main() {
   }
   await setCorrectCallFee(strategy, hardhat.network.name);
   console.log();
+  if (changeableParams.shouldHarvestOnDeposit) {
+    console.log(`Setting harvest on deposit: ${changeableParams.shouldHarvestOnDeposit}`);
+    await strategy.setHarvestOnDeposit(changeableParams.shouldHarvestOnDeposit);
+  }
 
   await Promise.all(verifyContractsPromises);
 
